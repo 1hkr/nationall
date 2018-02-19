848f080a6486ad68759119e5333e9c97309a5fad
lorene vignaud <lorene.vignaud@gmail.com>
2018-02-19 18:33:43 +0100

    no uniqueness needed for article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many :users, through: :donations
-  validates :title, uniqueness: true, presence: true
-  validates :subtitle, uniqueness: true, presence: true
-  validates :summary, :content, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
-  validates :picture, presence: true
+  validates :title, :subtitle, :summary, :content, :picture, :category, presence: true
+  validates :summary, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
   validates :category, inclusion: { in: ["politics","culture","europe","food","fashion"]}
 end
