ceb5cf6fe2e7efc11236732cc98742bde9643e9c
Takahiko Okada <takahikookada@gmail.com>
2018-02-19 18:29:24 +0100

    erased unnecessary comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,4 @@ Rails.application.routes.draw do
   resources :articles
   devise_for :users
   root to: 'articles#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
-
-
-# Rails.application.routes.draw do
-#   resources :cocktails do
-#     resources :doses, only:[:new, :create]
-#   end
-#   resources :doses, only:[:destroy]
-
-#   root to: 'cocktails#index'
-#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-# end
