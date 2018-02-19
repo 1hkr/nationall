f6bacd14978d9ec936d91a6ae583543a347fd51d
lorene vignaud <lorene.vignaud@gmail.com>
2018-02-19 18:16:10 +0100

    Tests done

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,8 +3,7 @@ class Article < ApplicationRecord
   has_many :users, through: :donations
   validates :title, uniqueness: true, presence: true
   validates :subtitle, uniqueness: true, presence: true
-  validates :summary, :content, uniqueness: true, length: { maximum: 1000,
-    too_long: "%{count} characters is the maximum allowed"
+  validates :summary, :content, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
   validates :picture, presence: true
-  validates :category, inclusion: { in: ("politics", "culture", "europe", "food", "fashion")}
+  validates :category, inclusion: { in: ["politics","culture","europe","food","fashion"]}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :articles
   has_many :articles, through: :donations
-  validates :bio, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed"
-  validates :first_name, :last_name, uniqueness: true
-  validates :city, presence: true
+  validates :bio, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
 end
