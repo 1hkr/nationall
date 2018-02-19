b3ec363e626ed34578d6d9ad6cc6ef5e16a26e97
Takahiko Okada <takahikookada@gmail.com>
2018-02-19 17:47:51 +0100

    basic structure of index and show done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+  resources :articles
   devise_for :users
-  root to: 'pages#home'
+  root to: 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+
+# Rails.application.routes.draw do
+#   resources :cocktails do
+#     resources :doses, only:[:new, :create]
+#   end
+#   resources :doses, only:[:destroy]
+
+#   root to: 'cocktails#index'
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
