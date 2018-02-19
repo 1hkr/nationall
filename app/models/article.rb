ce0ef90a65c1e45aa312577e3f0cd742a6bfe09a
lorene vignaud <lorene.vignaud@gmail.com>
2018-02-19 17:01:38 +0100

    Article validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many :users, through: :donations
+  validates :title, uniqueness: true, presence: true
+  validates :subtitle, uniqueness: true, presence: true
+  validates :summary, :content, uniqueness: true
+  validates :picture, presence: true
+  validates :category, inclusion: { in: ("politics", "culture", "europe", "food", "fashion")}
 end
