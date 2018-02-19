8532146f668364d328810c569b48a3a7c0a4c9e7
lorene vignaud <lorene.vignaud@gmail.com>
2018-02-19 17:33:11 +0100

    Validations done

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,5 @@
 class Donation < ApplicationRecord
   belongs_to :user
   belongs_to :article
+  validates :amount, inclusion: { in: (1..10000).to_a.map { |number| number/10.0 } }, numericality: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :articles
   has_many :articles, through: :donations
+  validates :bio, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed"
+  validates :first_name, :last_name, uniqueness: true
+  validates :city, presence: true
 end
