2b5a8d1cd773a0489053826272c9bf81a43dd06a
Takahiko Okada <takahikookada@gmail.com>
2018-02-20 12:03:39 +0100

    created basic structure

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,41 @@
 class ArticlesController < ApplicationController
-  before_action :find_article, only: [:show]
+  before_action :find_article, only: [:show, :edit, :destroy]
 
   def index
     @articles = Article.all
   end
 
   def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+    if @article.save
+      redirect_to articles_path, notice: 'Article was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: 'Article was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_url, notice: 'Article was successfully destroyed.'
   end
 
   private
