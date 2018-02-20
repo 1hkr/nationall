11c8cfac6c0cf7d2b715a9d547616368fb334a3a
Alexandre Gorius <ihkr@orange.fr>
2018-02-20 12:19:01 +0100

    Policies still in progress

--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -1,7 +1,21 @@
 class ArticlePolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope
+      scope.all
     end
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    record.user == user
+    # - record: the restaurant passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+  end
+
+  def destroy?
+    record.user == user
   end
 end
