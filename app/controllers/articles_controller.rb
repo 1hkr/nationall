138f31497fa893169743ec94872d4bd372d48152
lorene vignaud <lorene.vignaud@gmail.com>
2018-02-23 11:41:32 +0100

    Add dropdown list for category but REPEATED

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :find_article, only: [:show, :edit, :update, :destroy]
 
+
   def index
     @users = User.where.not(latitude: nil, longitude: nil)
     @markers = @users.map do |user|
